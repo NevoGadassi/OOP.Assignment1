fdf334828476de6339b5c600d24d1ea7590fc917
Nevo Gadassi <Nevogad4@gmail.com>
2022-12-29 00:29:09 +0200

    new documentation

--- src/main/java/observer/GroupAdmin.java
+++ src/main/java/observer/GroupAdmin.java
@@ -3,14 +3,23 @@ package observer;
 import java.util.ArrayList;
 
 public class GroupAdmin implements  Sender{
-    private UndoableStringBuilder str;
-    private ArrayList<Member> allmember;
+    private UndoableStringBuilder str; /** the UndoableStringBuilder of admin*/
 
+    private ArrayList<Member> allmember;/** list represent all the member that connected to admin*/
+
+    /**
+     constructor for group admin
+     */
     public GroupAdmin() {
         this.allmember = new ArrayList<>();
         this.str = new UndoableStringBuilder();
     }
 
+    /**
+     *
+     * function that writes new member to admin
+     * @param obj a new member to register to the list of member
+     */
     @Override
     public void register(Member obj) {
         if(!(allmember.contains(obj))){
@@ -18,6 +27,11 @@ public class GroupAdmin implements  Sender{
         }
     }
 
+    /**
+     * function that unregisters the member from the admins list
+     *
+     * @param obj object that represents the member we want to unregister
+     */
     @Override
     public void unregister(Member obj) {
         if(allmember.contains(obj)){
@@ -27,6 +41,9 @@ public class GroupAdmin implements  Sender{
 
     }
 
+    /**
+     * function that updates all the list of members
+     */
     public void updatemember()
     {
         for(int i=0;i<allmember.size();i++){
@@ -35,30 +52,68 @@ public class GroupAdmin implements  Sender{
     }
 
 
+    /**
+     *
+     * The java.lang.StringBuilder.insert(int offset, String str) method inserts the string into this character sequence.
+     * The characters of the String argument are inserted, in order, into this sequence at the indicated offset,
+     * moving up any characters originally above that position and increasing the length of this sequence by the length of the argument.
+     * we add  the new StringBuilder object to the List we made to save it and undo if needed.
+      * and after we update the members with the new undoablestringbuilder
+     * @param offset the place we add the  string obj
+     * @param obj   the string we want to add
+     */
     @Override
     public void insert(int offset, String obj) {
         this.str.insert(offset,obj);
         updatemember();
 
     }
-
+    /**
+     * The java.lang.StringBuilder.append(String str) method appends the specified string to this character sequence.
+     * The characters of the String argument are appended, in order, increasing the length of this sequence by the length of the argument.
+     * we add  the new StringBuilder object to the List we made to save it and undo if needed.
+    * and after we update the members with the new undoablestringbuilder
+     * * @param obj   the string we want to add
+     */
     @Override
     public void append(String obj) {
         this.str.append(obj);
         updatemember();
     }
 
+    /**
+     * The java.lang.StringBuilder.delete() method removes the characters in a substring of this sequence.
+     * The substring begins at the specified start and extends to the character at
+     * index end - 1 or to the end of the sequence if no such character exists.
+     * If start is equal to end, no changes are made.
+     * we add  the new StringBuilder object to the List we made to save it and undo if needed.
+     *    * and after we update the members with the new undoablestringbuilder
+     * @param start   from this place  we want delete
+     * @param end  until this place  we want delete
+     */
     @Override
     public void delete(int start, int end) {
         this.str.delete(start,end);
         updatemember();
     }
 
+    /**
+     * The UndoableStringBuilder.undo() method uses the List<StringBuilder> object we made and added a "save" to the list
+     * every time we used a method to undo the last method if used as it removes the last object we added to the list on the last method
+     * we used
+     * this creates the undo effect.
+     * and after we update the members with the new undoablestringbuilder
+     */
     @Override
     public void undo() {
         this.str.undo();
         updatemember();
     }
+
+    /**
+     *
+     * @return this UndoableStringBuilder
+     */
     public UndoableStringBuilder getstr(){
         return  this.str;
     }
--- src/test/java/observer/ConcreteMemberTest.java
+++ src/test/java/observer/ConcreteMemberTest.java
@@ -2,12 +2,17 @@ package observer;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class ConcreteMemberTest {
 
     @Test
     void update() {
         ConcreteMember member =new ConcreteMember();
         UndoableStringBuilder str=new UndoableStringBuilder();
+        str.append("text updated");
+        member.update(str);
+        assertEquals(member.Getstr().toString(),"text updated");
 
     }
 }
