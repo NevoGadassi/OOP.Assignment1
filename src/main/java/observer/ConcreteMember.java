2709c89497e6dd2f5b51f90a09c588731e614f30
Nevo Gadassi <Nevogad4@gmail.com>
2022-12-29 00:32:12 +0200

    new documentation

--- src/main/java/observer/ConcreteMember.java
+++ src/main/java/observer/ConcreteMember.java
@@ -4,14 +4,26 @@ public class ConcreteMember implements Member {
 
     private  UndoableStringBuilder str;
 
+    /**
+     constructor for ConcreteMember
+     */
     public ConcreteMember() {
         this.str = new UndoableStringBuilder();
     }
+
+    /**
+     *
+     * @return this member UndoableStringBuilder
+     */
     public UndoableStringBuilder Getstr()
     {
         return this.str;
     }
 
+    /**
+     * update this specific member with new UndoableStringBuilder
+     * @param usb the UndoableStringBuilder we update in this member
+     */
     @Override
     public void update(UndoableStringBuilder usb) {
         this.str=usb;
